79e7ef74320ea93d3736761e887086912406cea9
Daria <Roby@air-di-roberta.home>
2017-06-20 15:44:32 +0200

    ultime modifiche SecurityConfig

--- galleria/src/main/java/it/uniroma3/galleria/security/SecurityConfig.java
+++ galleria/src/main/java/it/uniroma3/galleria/security/SecurityConfig.java
@@ -25,7 +25,7 @@ auth.jdbcAuthentication().dataSource(dataSource)
     @Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/","/css/**", "/js/**", "/img/**","/vendor/**","/less/**","/mail/**","/signUp","/stanze","/opera/resultsOpera","/autore/resultsAutoreUtente","/opere","/autori","/stanza/resultsStanza")
+		.antMatchers("/","/css/**", "/js/**", "/img/**","/vendor/**","/less/**","/mail/**","/signUp","/stanze","/opera/resultsOpera","/autore/resultsAutoreUtente","/opere","/autori","/stanza/resultsStanza","/index.html")
 		.permitAll().antMatchers("/admin/**").hasRole("ADMIN")
 				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
 				.permitAll();
